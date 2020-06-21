fd304b230200c27a98793de482527b098a9e97b3
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-20 23:56:40 -0400

    Login template version 2.5. Update to dependencies for login page. Compass is depreciated so trying a different style

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,3 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
-
-mix.sass('resources/sass/login_register.scss', 'public/css/login.css');
