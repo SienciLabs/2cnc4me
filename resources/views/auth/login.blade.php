fc404f5b955c6968451f39846c3485273ba1a22d
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-22 19:52:37 -0400

    Added new text to login and padding

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="container login-container">
         <div class="row">
             <div class="col-md-6 login-form-1">
-                <h3>Login for Form 1</h3>
+                <h3>Login</h3>
                 <form>
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Your Email *" value="" />
@@ -22,13 +22,19 @@
                 </form>
             </div>
             <div class="col-md-6 login-form-2">
-                <h3>Login for Form 2</h3>
+                <h3>Sign Up</h3>
                 <form>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                        <input type="text" class="form-control" placeholder="Your Username" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Your Email" value="" />
                     </div>
                     <div class="form-group">
                         <input type="password" class="form-control" placeholder="Your Password *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Your Confirmed Password *" value="" />
                     </div>
                     <div class="form-group">
                         <input type="submit" class="btnSubmit" value="Login" />
