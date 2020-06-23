bbb4f80d08b4bd29e618d273eff538f07351e7f7
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 10:27:58 -0400

    minor ui with cards v6

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -9,30 +9,41 @@
         <option value="difficulty">Easiest</option>
     </select>--}}
 
-    <div class="row">
+    <div class="card-deck">
         <h2><span class="label label-default">Categories</span></h2>
-        <div class="col-sm-6">
-            <div class="card text-center">
-                <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}" >
-                <div class="card-body">
-                    <h2><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
-                            Science</a></h2>
-                </div>
-
+        <div class="card text-center">
+            <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}" >
+            <div class="card-body">
+                <h2><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
+                        Science</a></h2>
             </div>
-        </div>
-        <div class="col-sm-6">
 
-            <div class="card text-center">
-                <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
+        </div>
+        <div class="card text-center">
+            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
                 ">
-                <div class="card-body">
-                    <h3><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
-                            Art</a></h3>
-                </div>
+            <div class="card-body">
+                <h3><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
+                        Art</a></h3>
             </div>
         </div>
+        <div class="card text-center">
+            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
+                ">
+            <div class="card-body">
+                <h3><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
+                        French</a></h3>
+            </div>
+        </div>
+
+        <div class="card text-center">
+            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
+                ">
+            <div class="card-body">
+                <h3><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
+                        Science</a></h3>
+            </div>
+        </div>
+
     </div>
-
-
 </x-app>
