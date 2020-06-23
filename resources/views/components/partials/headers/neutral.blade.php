fc9628b0a1cb714577e67d3c0b83a8c550d73b52
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 04:43:49 -0400

    Adding content to welcome page v4

--- resources/views/components/partials/headers/neutral.blade.php
+++ resources/views/components/partials/headers/neutral.blade.php
@@ -2,7 +2,7 @@
 <header class="">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('index.html') }}"><h2><img src="/images/24logo.svg" alt="2cnc4me" width="30px"></h2></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><h2><img src="/images/24logo.svg" alt="2cnc4me" width="30px"></h2></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -221,7 +221,7 @@
         <div class="card-deck" style="padding: 30px">
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-">
+                ">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -243,7 +243,7 @@
             </div>
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-">
+                ">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
