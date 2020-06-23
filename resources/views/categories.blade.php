1aca3a81647e81205a17690ecec733601b1ddcc5
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 10:29:31 -0400

    minor ui with cards v7

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -8,17 +8,10 @@
         <option value="ratings">Highest Rated</option>
         <option value="difficulty">Easiest</option>
     </select>--}}
+    <br>
+    <h2><span class="label label-default">Categories</span></h2>
 
     <div class="card-deck">
-        <h2><span class="label label-default">Categories</span></h2>
-        <div class="card text-center">
-            <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}" >
-            <div class="card-body">
-                <h2><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
-                        Science</a></h2>
-            </div>
-
-        </div>
         <div class="card text-center">
             <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
                 ">
