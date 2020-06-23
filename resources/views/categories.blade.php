71757f73b1cfa2978deb9ed9d4a404ae4f36635f
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 09:45:23 -0400

    label rework basic attempt 10.4

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -12,25 +12,26 @@
 
     <div class="container" style="margin-top: 2.5%">
         <h2><span class="label label-default">Categories</span></h2>
-{{--
         <div class="card-deck" style="padding: 30px">
+            <div class="categories-card">
+                <img src="{{ asset('/images/categories/Art.svg')}}" alt="Avatar" style="width:100%">
+                <div class="container">
+                    <h4><b>Art</b></h4>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
 
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}">
                 <h2 class="card-title" style="text-align: center">Categories</h2>
+            </div>
 
-            </div>
-            <div class="card">
-                <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
                 ">
@@ -43,14 +44,6 @@
                 </div>
             </div>
         </div>
---}}
-        <div class="categories-card">
-            <img src="{{ asset('/images/categories/Art.svg')}}" alt="Avatar" style="width:100%">
-            <div class="container">
-                <h4><b>Art</b></h4>
-            </div>
-        </div>
-
     </div>
 
 </x-app>
