e76578bf325c62c8537263627d4c112f9c1b0499
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 10:16:46 -0400

    minor ui with cards v2

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -13,23 +13,22 @@
         <h2><span class="label label-default">Categories</span></h2>
         <div class="card-deck" style="padding: 30px">
 
-            <div class="card text-center w-50">
+            <div class="card text-center">
                 <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}" >
                 <div class="card-body">
-                    <a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
-                        Categories</a>
+                    <h2><a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
+                            Categories</a></h2>
                 </div>
 
             </div>
 
-            <div class="card text-center w-50">
+            <div class="card text-center">
                 <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
                 ">
                 <div class="card-body">
                     <a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}">
                         Another Cat</a>
                 </div>
-
             </div>
         </div>
     </div>
