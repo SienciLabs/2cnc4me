6ca337fdc138cffa502d5e759b3cc8734830370d
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 02:41:43 -0400

    Button in header trial 1

--- resources/views/components/partials/headers/neutral.blade.php
+++ resources/views/components/partials/headers/neutral.blade.php
@@ -20,8 +20,12 @@
                         <a class="nav-link" href="../blog.html">Collections</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('login')}}">Login/Signup</a>
-                    </li>
+                        <button type="submit"
+                                class="bg-blue-500 hover:bg-blue-600 rounded-lg shadow px-10 text-sm text-white"
+                        >
+                            Cut!
+                        </button>
+{{--                        <a class="nav-link" href="{{route('login')}}">Login/Signup</a>--}}
                     </li>
                 </ul>
             </div>
