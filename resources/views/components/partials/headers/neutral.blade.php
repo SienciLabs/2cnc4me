4dabfff845c3ada83cb4e6b65c78a5becb135d2d
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 08:39:48 -0400

    Work on categories page v3

--- resources/views/components/partials/headers/neutral.blade.php
+++ resources/views/components/partials/headers/neutral.blade.php
@@ -14,7 +14,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link"href="{{ route('categories') }}">Categories</a>
+                        <a class="nav-link"href="{{ route('categories/') }}">Categories</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../blog.html">Collections</a>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('categories', function () {
+Route::get('/categories', function () {
     return view('categories');
 });
 
