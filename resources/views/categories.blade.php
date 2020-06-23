a62b01702645a94933eb9c7173dc992a8fd49392
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 10:05:07 -0400

    just messing around with cards v3

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -8,22 +8,16 @@
         <option value="ratings">Highest Rated</option>
         <option value="difficulty">Easiest</option>
     </select>--}}
-    <br><br>
 
     <div class="container" style="margin-top: 2.5%">
         <h2><span class="label label-default">Categories</span></h2>
         <div class="card-deck" style="padding: 30px">
-            <div class="categories-card">
-                <img src="{{ asset('/images/categories/Art.svg')}}" alt="Avatar" style="width:100%">
-                <div class="container" style="text-align: center">
-                    <a href="{{ url('https://www.merriam-webster.com/dictionary/category') }}"><b>Art</b></a>
-                </div>
-            </div>
+
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}" >
                 <div class="card-body">
                     <a class="card-title" href="{{ url('https://www.merriam-webster.com/dictionary/category') }}"
-                       style="text-align: center">Card title</a>
+                       style="text-align: center">Categories</a>
                 </div>
 
             </div>
