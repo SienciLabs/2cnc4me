40393a0ce4f8435ed50f9c1e79b70e988f103f2a
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 09:22:43 -0400

    label rework basic attempt 8.5

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -11,7 +11,6 @@
     <br><br>
 
     <div class="container">
-        <span class="label label-default page-label"><h2>Categories</h2></span>
         <div class="card-deck" style="padding: 30px">
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="{{ asset('/images/categories/Art.svg')}}">
