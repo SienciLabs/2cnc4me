daaf7e984261d06899830853b5a5fa9480f9ad58
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-23 22:11:47 -0400

    Possible fix to routing v7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-app>
 
-    <div class="container">
+    {{--<div class="container">
         <div class="card-deck" style="padding: 30px">
             <div class="card">
                 <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
@@ -36,5 +36,121 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
+    <!-- Banner Starts Here -->
+        <div class="main-banner header-text">
+            <div class="container-fluid">
+                <div class="owl-banner owl-carousel">
+                    <div class="item">
+                        <img src="{{asset('/images/CNSheep.jpg')}}" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Office</span>
+                                </div>
+                                <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Furniture</span>
+                                </div>
+                                <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Office</span>
+                                </div>
+                                <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Furniture</span>
+                                </div>
+                                <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Office</span>
+                                </div>
+                                <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Furniture</span>
+                                </div>
+                                <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Banner Ends Here -->
 </x-app>
