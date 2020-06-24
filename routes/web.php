85013379ea6407512e034fa4fa9a5485cf057658
ConnorEALynch <empirepenguins@gmail.com>
2020-06-24 09:14:48 -0700

    resolved routes/web.php merge conflict

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-<<<<<<< HEAD
-    return 'this is a test';
-=======
     return view('welcome');
->>>>>>> parent of f3bff6ef... changed welcome page and undid previous auth commenting. it was not the issue
+
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
+
 
 Route::resource('posts', 'PostController');
-
-Route::get('/test', function() {return "Hello world";});
 
 Route::get('/categories', function (){
     return view('categories');
 });
-=======
->>>>>>> parent of 5d2f3600... changed some routing stuff
+
