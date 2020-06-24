2e934019307a548ed5786b098a8caac7107470c3
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-24 15:31:55 -0400

    Potential routing trial 1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                         <input type="password" class="form-control" placeholder="Password" value="" />
                     </div>
                     <div class="form-group">
-                        <input type="submit" class="btnSubmit" value="Login" />
+                        <input type="submit" class="btnSubmit" href="{{ url('home') }}" value="Login" />
                     </div>
                     <div class="form-group">
                         <a href="#" class="ForgetPwd">Forget Password?</a>
