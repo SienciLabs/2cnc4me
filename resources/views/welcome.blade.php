2a6b4b9f6039b2f9f2f845fc4e738ca6bcd6f216
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-25 00:46:36 -0400

    Welcome page new version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,18 @@
 <x-app>
-    Welcome To 2cnc4me.com
+{{--    Feature header--}}
+    <div class="heading-page header-text">
+        <section class="page-heading">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-content">
+                            <h3>
+                               Featured
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </x-app>
