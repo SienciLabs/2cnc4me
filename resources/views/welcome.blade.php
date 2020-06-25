a5e87414bb35f9989056fab3184ad755fdbacfb4
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-25 03:11:12 -0400

    adding more content v4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,113 +15,116 @@
             </div>
         </section>
         <!-- Banner Starts Here -->
-        <div class="container-fluid">
-            <div class="owl-banner owl-carousel">
-                <div class="item">
-                    <img src="{{asset('/images/CNSheep.jpg')}}" alt="">
-                    <div class="item-content">
-                        <div class="main-content">
-                            <div class="meta-category">
-                                <span>Office</span>
+
+        <div class="main-banner header-text">
+            <div class="container-fluid">
+                <div class="owl-banner owl-carousel">
+                    <div class="item">
+                        <img src="{{asset('/images/CNSheep.jpg')}}" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Office</span>
+                                </div>
+                                <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
                             </div>
-                            <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
-                            <ul class="post-info">
-                                <li><a href="#">cncguy94</a></li>
-                                <li><a href="#">Office</a></li>
-                                <li><a href="#">Add to Collection</a></li>
-                                <li><a href="#">38 Likes</a></li>
-                                <li><a href="#">144 Downloads</a></li>
-                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="/images/CNSheep.jpg" alt="">
-                    <div class="item-content">
-                        <div class="main-content">
-                            <div class="meta-category">
-                                <span>Furniture</span>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Furniture</span>
+                                </div>
+                                <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
                             </div>
-                            <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
-                            <ul class="post-info">
-                                <li><a href="#">cncguy94</a></li>
-                                <li><a href="#">Office</a></li>
-                                <li><a href="#">Add to Collection</a></li>
-                                <li><a href="#">38 Likes</a></li>
-                                <li><a href="#">144 Downloads</a></li>
-                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="/images/CNSheep.jpg" alt="">
-                    <div class="item-content">
-                        <div class="main-content">
-                            <div class="meta-category">
-                                <span>Office</span>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Office</span>
+                                </div>
+                                <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
                             </div>
-                            <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
-                            <ul class="post-info">
-                                <li><a href="#">cncguy94</a></li>
-                                <li><a href="#">Office</a></li>
-                                <li><a href="#">Add to Collection</a></li>
-                                <li><a href="#">38 Likes</a></li>
-                                <li><a href="#">144 Downloads</a></li>
-                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="/images/CNSheep.jpg" alt="">
-                    <div class="item-content">
-                        <div class="main-content">
-                            <div class="meta-category">
-                                <span>Furniture</span>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Furniture</span>
+                                </div>
+                                <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
                             </div>
-                            <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
-                            <ul class="post-info">
-                                <li><a href="#">cncguy94</a></li>
-                                <li><a href="#">Office</a></li>
-                                <li><a href="#">Add to Collection</a></li>
-                                <li><a href="#">38 Likes</a></li>
-                                <li><a href="#">144 Downloads</a></li>
-                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="/images/CNSheep.jpg" alt="">
-                    <div class="item-content">
-                        <div class="main-content">
-                            <div class="meta-category">
-                                <span>Office</span>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Office</span>
+                                </div>
+                                <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
                             </div>
-                            <a href="post-details.html"><h4>Simple Desk Organizer</h4></a>
-                            <ul class="post-info">
-                                <li><a href="#">cncguy94</a></li>
-                                <li><a href="#">Office</a></li>
-                                <li><a href="#">Add to Collection</a></li>
-                                <li><a href="#">38 Likes</a></li>
-                                <li><a href="#">144 Downloads</a></li>
-                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="item">
-                    <img src="/images/CNSheep.jpg" alt="">
-                    <div class="item-content">
-                        <div class="main-content">
-                            <div class="meta-category">
-                                <span>Furniture</span>
+                    <div class="item">
+                        <img src="/images/CNSheep.jpg" alt="">
+                        <div class="item-content">
+                            <div class="main-content">
+                                <div class="meta-category">
+                                    <span>Furniture</span>
+                                </div>
+                                <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
+                                <ul class="post-info">
+                                    <li><a href="#">cncguy94</a></li>
+                                    <li><a href="#">Office</a></li>
+                                    <li><a href="#">Add to Collection</a></li>
+                                    <li><a href="#">38 Likes</a></li>
+                                    <li><a href="#">144 Downloads</a></li>
+                                </ul>
                             </div>
-                            <a href="post-details.html"><h4>Modular Cabinets with Plastic keys</h4></a>
-                            <ul class="post-info">
-                                <li><a href="#">cncguy94</a></li>
-                                <li><a href="#">Office</a></li>
-                                <li><a href="#">Add to Collection</a></li>
-                                <li><a href="#">38 Likes</a></li>
-                                <li><a href="#">144 Downloads</a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
