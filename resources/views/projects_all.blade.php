c33b32da20309ab1b169c3504e8805595dc91d5f
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-25 03:40:15 -0400

    Project page change

--- resources/views/projects_all.blade.php
+++ resources/views/projects_all.blade.php
@@ -1,4 +1,18 @@
 <x-app>
-    <br>
-    <h2><span class="label label-default pt-lg-5">Projects</span></h2>
+    {{--    Project header --}}
+    <div class="heading-page header-text">
+        <section class="page-heading">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-content">
+                            <h4>
+                                Featured
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </x-app>
