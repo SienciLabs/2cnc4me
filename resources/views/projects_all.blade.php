9153d890ddb5f4598fd24ef402e42e9aef6e858a
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-25 03:51:54 -0400

    Project page change v3

--- resources/views/projects_all.blade.php
+++ resources/views/projects_all.blade.php
@@ -6,7 +6,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-content">
-                            <h4>
+                            <h4 style="color: black;">
                                 Project
                             </h4>
                         </div>
@@ -15,4 +15,62 @@
             </div>
         </section>
     </div>
+
+    <section class="blog-post grid-system">
+        <div class="container">
+            <div class="row">
+                <div class="all-blog-posts">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <img alt="Card image cap" class="card-img-top"
+                                     src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img alt="Card image cap" class="card-img-top"
+                                     src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img alt="Card image cap" class="card-img-top"
+                                     src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img alt="Card image cap" class="card-img-top"
+                                     src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
 </x-app>
