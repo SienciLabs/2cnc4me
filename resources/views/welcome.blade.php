f1ebc8b35f4ba8ce78d6d9d6509d945d8ad11c8f
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 02:12:32 -0400

    Carousel test v4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -178,19 +178,17 @@
                                         <div class="row carouselRow">
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-01.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 1</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-04.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 2</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
@@ -198,19 +196,17 @@
                                         <div class="row carouselRow">
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-02.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 1</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-03.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 2</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
@@ -221,19 +217,17 @@
                                         <div class="row carouselRow">
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-04.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 1</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-01.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 2</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
@@ -241,24 +235,23 @@
                                         <div class="row carouselRow">
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-03.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 1</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div class="card">
+                                                    <img class="card-img-top" src="/images/blog-thumb-02.jpg" alt="Card image cap">
                                                     <div class="card-body">
-                                                        <h4 class="card-title">Card 2</h4>
-                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
-                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                        <h4 class="card-text">Card 1</h4>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
+
 
                                 </div>
                             </div>
