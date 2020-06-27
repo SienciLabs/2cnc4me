71eb6afdf710ee5e400f62529830a95a4658e0e2
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 00:56:14 -0400

    Carousel test w/ buttons v3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -199,18 +199,15 @@
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="row">
-                                <div class="col-12">
-                                    <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
-                                        <span class="fa fa-chevron-left" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next text-dark" href="projectCarousel" role="button" data-slide="next">
-                                        <span class="fa fa-chevron-right" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </div>
+                            <div class="col-12">
+                                <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
+                                    <span class="fa fa-chevron-left" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next text-dark" href="projectCarousel" role="button" data-slide="next">
+                                    <span class="fa fa-chevron-right" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
                             </div>
                         </div>
                     </div>
