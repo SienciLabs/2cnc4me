b7a07312abac8219089f7393c97e3f2b73308bcf
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 01:48:29 -0400

    Carousel addition to collections section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -170,40 +170,7 @@
                                 Projects
                             </h4>
                         </div>
-{{--
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-01.jpg" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-02.jpg" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-03.jpg" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-04.jpg" alt="">
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
---}}
+                        {{--Carousel start--}}
                         <div class="row mx-auto my-auto">
                             <div id="projectCarousel" class="carousel slide w-100" data-ride="carousel">
                                 <div class="carousel-inner" role="listbox">
@@ -308,6 +275,8 @@
                                 </a>
                             </div>
                         </div>
+                        {{--Carousel end--}}
+
                     </div>
                 </div>
             </div>
@@ -343,36 +312,112 @@
                                 Collections
                             </h4>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-01.jpg" alt="">
+                        {{--Carousel start--}}
+                        <div class="row mx-auto my-auto">
+                            <div id="projectCarousel" class="carousel slide w-100" data-ride="carousel">
+                                <div class="carousel-inner" role="listbox">
+                                    <div class="carousel-item active">
+                                        <div class="row carouselRow">
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 1</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 2</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row carouselRow">
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 1</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 2</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-02.jpg" alt="">
+
+                                    <div class="carousel-item">
+                                        <div class="row carouselRow">
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 1</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 2</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row carouselRow">
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 1</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Card 2</h4>
+                                                        <p class="card-text">Some make up the bulk of the card's content.</p>
+                                                        <a href="#" class="btn btn-primary">Button</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-04.jpg" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="blog-post">
-                                    <div class="blog-thumb">
-                                        <img src="/images/blog-thumb-04.jpg" alt="">
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col">
+                                <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
+                                    <span class="fa fa-chevron-left" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next text-dark" href="#projectCarousel" role="button" data-slide="next">
+                                    <span class="fa fa-chevron-right" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
+                        {{--Carousel end--}}
                     </div>
                 </div>
             </div>
