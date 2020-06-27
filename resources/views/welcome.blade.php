e8738e532a61f14a3d90c83af5daa68c5fbbafe5
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 02:05:36 -0400

    Carousel test v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -264,16 +264,14 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col">
-                                <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
-                                    <span class="fa fa-chevron-left" aria-hidden="true"></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a class="carousel-control-next text-dark" href="#projectCarousel" role="button" data-slide="next">
-                                    <span class="fa fa-chevron-right" aria-hidden="true"></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </div>
+                            <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
+                                <span class="fa fa-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next text-dark" href="#projectCarousel" role="button" data-slide="next">
+                                <span class="fa fa-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
                         {{--Carousel end--}}
                     </div>
