13f25177ae1e18410ad8fcdbdf2d55d6ee82c04c
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 01:32:28 -0400

    Carousel V2 test 3.7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -249,6 +249,7 @@
                                             </div>
                                         </div>
                                     </div>
+
                                     <div class="carousel-item">
                                         <div class="row carouselRow">
                                             <div class="col-lg-6">
@@ -291,18 +292,22 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
-                                        <span class="fa fa-chevron-left" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next text-dark" href="#projectCarousel" role="button" data-slide="next">
-                                        <span class="fa fa-chevron-right" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
+
                                 </div>
                             </div>
                         </div>
-
+                        <div class="row">
+                            <div class="col">
+                                <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
+                                    <span class="fa fa-chevron-left" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next text-dark" href="#projectCarousel" role="button" data-slide="next">
+                                    <span class="fa fa-chevron-right" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
