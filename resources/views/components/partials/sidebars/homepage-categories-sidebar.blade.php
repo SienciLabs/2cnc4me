b4e567da859418527498004fb538592a1878ad78
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 17:20:35 -0400

    Sidebar test V1

---
+++ resources/views/components/partials/sidebars/homepage-categories-sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="sidebar-item categories">
+                <div class="sidebar-heading">
+                    <h2>Categories</h2>
+                </div>
+                <div class="content">
+                    <ul>
+                        <li><a href="#">Furniture</a> </li>
+                        <li><a href="#">Signs</a> </li>
+                        <li><a href="#">Joinery</a> </li>
+                        <li><a href="#">Games</a> </li>
+                        <li><a href="#">Toys</a> </li>
+                        <li><a href="#">Sculptures</a> </li>
+                        <li><a href="#">Kitchen</a> </li>
+                        <li><a href="#">CNC Mods</a> </li>
+                        <li><a href="#">Music</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
