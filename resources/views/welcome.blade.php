c4596590a8c55b086e4f2942a1d4caeda9938aa4
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 00:47:34 -0400

    Carousel test w/ buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -171,26 +171,19 @@
                             </h4>
                         </div>
                         <div class="row">
-                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <img class="d-block w-100" src="..." alt="First slide">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100" src="..." alt="Second slide">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100" src="..." alt="Third slide">
+                            <div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a class="carousel-control-prev text-dark" href="#projectCarousel" role="button" data-slide="prev">
+                                            <span class="fa fa-chevron-left" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next text-dark" href="projectCarousel" role="button" data-slide="next">
+                                            <span class="fa fa-chevron-right" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
                                     </div>
                                 </div>
-                                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
                             </div>
 {{--                            <div class="col-lg-6">
                                 <div class="blog-post">
