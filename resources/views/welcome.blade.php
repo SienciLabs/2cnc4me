30870f848b5025ae55996c742a76653e5b827d83
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-27 00:43:16 -0400

    Carousel test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -171,7 +171,28 @@
                             </h4>
                         </div>
                         <div class="row">
-                            <div class="col-lg-6">
+                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class="d-block w-100" src="..." alt="First slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="..." alt="Second slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="..." alt="Third slide">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+{{--                            <div class="col-lg-6">
                                 <div class="blog-post">
                                     <div class="blog-thumb">
                                         <img src="/images/blog-thumb-01.jpg" alt="">
@@ -198,7 +219,7 @@
                                         <img src="/images/blog-thumb-04.jpg" alt="">
                                     </div>
                                 </div>
-                            </div>
+                            </div>--}}
                         </div>
                     </div>
                 </div>
