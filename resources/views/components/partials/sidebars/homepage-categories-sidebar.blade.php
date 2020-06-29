3ab0da6232de4d310cea2d318e975a71f4f00dc2
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-28 23:59:35 -0400

    More categories test v1

--- resources/views/components/partials/sidebars/homepage-categories-sidebar.blade.php
+++ resources/views/components/partials/sidebars/homepage-categories-sidebar.blade.php
@@ -7,47 +7,47 @@
                 </div>
                 <div class="col">
                     <ul>
-                        <a href="#" >
+                        <a class="category-anchor" href="#">
                             <li class="category-list">
                                 <img src="/images/icons/category/Furniture.svg" class="category-icon">Furniture
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Signs.svg" class="category-icon">Signs
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Games.svg" class="category-icon">Games
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Toys.svg" class="category-icon">Toys
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Sculptures.svg" class="category-icon">Sculpture
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Art.svg" class="category-icon">Art
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/CNCMods.svg" class="category-icon">CNC Mods
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Home.svg" class="category-icon">Home
                             </li>
                         </a>
-                        <a href="#" >
+                        <a class="category-anchor" href="#" >
                             <li class="category-list">
                                 <img src="/images/icons/category/Music.svg" class="category-icon">Music
                             </li>
