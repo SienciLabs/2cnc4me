e988bd08eb6671852522b365565dae2f4a196048
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-01 04:35:13 -0400

    Basic

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -18,7 +18,22 @@
 
     <div class="card-deck" style="padding: 30px 350px 30px 360px;">
 
-
+        <div class="project-card-wrapper">
+            <div class="card">
+                <img alt="Card image cap" class="card-img-top"
+                     src="https://api.adorable.io/avatars/285/abott@adorable.png">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">Ball Maze</div>
+                        <div class="col">38 Likes</div>
+                    </div>
+                    <div class="row">
+                        <div class="col">By CNCGuy</div>
+                        <div class="col">144 Downloads</div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 
