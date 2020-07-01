1dae2c4e3d287904e37d38b7d9271adf8f150cc3
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-01 04:30:29 -0400

    HARD --RESET. START from Card-deck

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -17,70 +17,8 @@
     </div>
 
     <div class="card-deck" style="padding: 30px 350px 30px 360px;">
-        {{--<div class="card project-card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body project-card-body" style="height: 60px; background-color: #363636">
-                <div class="row" style="color: white">
-                    <div class="col">
-                        Ball Maze
-                    </div>
-                    <div class="col" style="text-align: right">
-                        38 Likes
-                    </div>
-                </div>
-                <div class="row" style="color: white">
-                    <div class="col">
-                        By CNCGuy
-                    </div>
-                    <div class="col" style="text-align: right">
-                        144 Downloads
-                    </div>
-                </div>
-            </div>
 
-            <div class="card-footer project-card-footer">
-                <div class="row" style="color: white">
-                    <div class="col">
-                        Toys & Games
-                    </div>
-                    <div class="col" style="text-align: right">
-                        Add To Collection
-                    </div>
-                </div>
-            </div>
-        </div>--}}
 
-        <div class="project-card-wrapper">
-            <div class="project-card">
-                <div class="project-card-img-wrapper">
-                    <img alt="Card image cap" class="card-img-top"
-                         src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard">
-                </div>
-{{--                <div class="project-card-body">
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. </p>
-                </div>--}}
-                <div class="project-card-body">
-                    <div class="row" style="color: white">
-                        <div class="col">
-                            Ball Maze
-                        </div>
-                        <div class="col" style="text-align: right">
-                            38 Likes
-                        </div>
-                    </div>
-                    <div class="row" style="color: white">
-                        <div class="col">
-                            By CNCGuy
-                        </div>
-                        <div class="col" style="text-align: right">
-                            144 Downloads
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="btn btn-primary">Go somewhere New</a>
-            </div>
-        </div>
 
     </div>
 
