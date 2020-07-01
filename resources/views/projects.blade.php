9256982dae585e8d5fb084a251b7fcec12b8392b
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-06-30 20:54:46 -0400

    Hover 3.0

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -17,7 +17,7 @@
     </div>
 
     <div class="card-deck" style="padding: 30px 350px 30px 360px;">
-        <div class="card project-card">
+        {{--<div class="card project-card">
             <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
                 ">
             <div class="card-body project-card-body" style="height: 60px; background-color: #363636">
@@ -49,69 +49,20 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
+        </div>--}}
+
+        <div class="project-card-wrapper col-lg-4 col-md-6 col-xs-12">
+            <div class="project-card">
+                <div class="project-card-img-wrapper">
+                    <img class="project-card-img-top" src="https://www.fortech.ro/wp-content/uploads/2018/12/DigitalCommerceSoftwarePlatform-e1554472385425.jpg" alt="Card image cap">
+                </div>
+                <div class="project-card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content. </p>
+                </div>
+                <a href="#" class="btn btn-primary">Go somewhere New</a>
             </div>
         </div>
 
     </div>
-    <div class="card-deck" style="padding: 30px 350px 30px 360px;">
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
 
-    </div>
-    <div class="card-deck" style="padding: 30px 350px 30px 360px;">
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
-        <div class="card">
-            <img alt="Card image cap" class="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard
-                ">
-            <div class="card-body">
-                <h5 class="card-title" style="text-align: center">Title</h5>
-            </div>
-        </div>
-
-    </div>
 </x-app>
