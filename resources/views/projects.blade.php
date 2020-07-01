0b631eed29628be64b75e7a3f3ff4b35d2498749
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-01 09:02:22 -0400

    putting things into partial

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -18,57 +18,9 @@
 
     <div class="card-deck" style="padding: 30px 350px 30px 360px;">
 
-        <div class="project-card-wrapper">
-            <div class="project-card">
-                <a href="#" class="stretched-link">
-                    <div class="project-card-img-wrapper">
-                        <img alt="Card image cap" class="project-card-image"
-                              src="/images/Ballmaze.JPG">
-                    </div>
-                    <div class="project-card-body">
-                        <div class="card-text" >
-                            <div class=" row">
-                                <div class="col" style="padding-left: 20px;">
-                                    Ball Maze
-                                </div>
-                                <div style="padding-right: 35px;" class="col-md-auto">
-                                    38
-                                    <img class="project-icons" src="/images/icons/LikeIcon.svg">
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col" style="padding-left: 20px;">
-                                    <img class="project-icons" src="/images/icons/UserIcon.svg">
-                                    By CNCGuy
-                                </div>
-                                <div style="padding-right: 35px;" class="col-md-auto">
-                                    144
-                                    <img class="project-icons" src="/images/icons/DownloadIcon.svg">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-
-                <div class="project-card-footer" style="border-top: 2px solid #F2F2F2">
-                    <div class="row">
-                        <a href="{{ url('categories') }}">
-                            <div class="col" style="padding-left: 20px;">
-                                <img class="project-icons" src="/images/icons/UserIcon.svg">
-                                Category
-                            </div>
-                        </a>
-                        <a href="#" class="stretched-link">
-                            <div style="padding-right: 30px;" class="col-md-auto">
-                                <img class="project-icons" src="/images/icons/PlusIcon.svg">
-                                Add To Collection
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('components.partials.cards.project-cards')
+        @include('components.partials.cards.project-cards')
+        @include('components.partials.cards.project-cards')
 
     </div>
 
