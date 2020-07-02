b99017e09b683d5a45f05306c363d03dbca9d8f3
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-02 02:12:42 -0400

    Img grid test with new collection card

---
+++ resources/views/components/partials/cards/collection-cards.blade.php
@@ -0,0 +1,38 @@
+<div class="project-card-wrapper">
+    <div class="project-card">
+        <a href="#" class="stretched-link">
+            <div class="img-grid">
+                <div class="img-wrapper">
+                    <img alt="Card image cap"
+                         src="/images/Ballmaze.JPG">
+                </div>
+                <div class="img-wrapper">
+                    <img alt="Card image cap"
+                         src="/images/Ballmaze.JPG">
+                </div>
+            </div>
+            <div class="project-card-body">
+                <div class="card-text" >
+                    <div class=" row">
+                        <div class="col" style="padding-left: 20px;">
+                            Ball Maze
+                        </div>
+                        <div style="padding-right: 35px;" class="col-md-auto">
+                            38
+                            <img class="project-icons" src="/images/icons/LikeIcon.svg">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col" style="padding-left: 20px;">
+                            <img class="project-icons" src="/images/icons/UserIcon.svg">
+                            By CNCGuy
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
+
+
