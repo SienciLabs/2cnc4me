89a614b60d1e5444eea7f0dce9ddcb47cffe7092
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-03 16:11:00 -0400

    Sub menu with script fix

--- resources/views/components/partials/footers/footer-scripts.blade.php
+++ resources/views/components/partials/footers/footer-scripts.blade.php
@@ -19,7 +19,7 @@
     }
 </script>
 {{--Nav-bar dropdown script--}}
-<script>
+<script language="text/Javascript">
     /* When the user clicks on the button,
     toggle between hiding and showing the dropdown content */
     function myFunction() {
