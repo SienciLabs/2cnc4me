21ec7442e512498aac67a6447b4a6f7a4a96d77a
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-03 16:43:20 -0400

    Just added script to master

--- resources/views/components/partials/footers/footer-scripts.blade.php
+++ resources/views/components/partials/footers/footer-scripts.blade.php
@@ -8,7 +8,7 @@
 <script src="/js/slick.js"></script>
 <script src="/js/isotope.js"></script>
 <script src="/js/accordions.js"></script>
-{{--<script type = "text/Javascript">
+<script type = "text/Javascript">
     cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
     function clearField(t){                   //declaring the array outside of the
         if(! cleared[t.id]){                      // function makes it static and global
@@ -17,22 +17,5 @@
             t.style.color='#fff';
         }
     }
-</script>--}}
-{{--Nav-bar dropdown script--}}
-<script type="text/javascript">
-    /* When the user clicks on the button,
-    toggle between hiding and showing the dropdown content */
-    function ddFunc() {
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-
-    // Close the dropdown if the user clicks outside of it
-    window.onclick = function(e) {
-        if (!e.target.matches('.dropbtn')) {
-            var myDropdown = document.getElementById("myDropdown");
-            if (myDropdown.classList.contains('show')) {
-                myDropdown.classList.remove('show');
-            }
-        }
-    }
 </script>
+
