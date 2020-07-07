003b87d44d2ed30f18f7c25cb4ab4e749b90f712
Samuel Lagunju <GitOfSamuel@gmail.com>
2020-07-07 15:47:20 -0400

    new dropdown func test 1.0

--- resources/views/components/partials/nav-headers/loggedIn.blade.php
+++ resources/views/components/partials/nav-headers/loggedIn.blade.php
@@ -19,14 +19,17 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('collections') }}">Collections</a>
                     </li>
-                    <li class="nav-item">
-                        <buton class="dropbtn"  onClick="myFunction();"><img src="{{ asset('images/icons/PlusIcon.svg') }}" alt="Add" width="30px"></buton>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
                     </li>
-                        <ul class="dropdown-content" id="myDropdown">
-                            <a> Add New Project </a>
-                            <a> Add New Collection </a>
-                        </ul>
-{{--                        <a class="nav-link" href="../post-details.html">Add</a>--}}
                     <li class="nav-item">
                         <a class="nav-link" href="../post-details.html">Profile</a>
                     </li>
