b5f2d1cc450ff24d30ee39452dc99e31300559f5
ConnorEALynch <empirepenguins@gmail.com>
2020-07-07 17:15:00 -0700

    minor fixes and added DB migration

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\project;
+use App\Project;
 use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
--- app/Project.php
+++ app/Project.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
 	use SoftDeletes;
-	protected $table = 'posts';
+	protected $table = 'project';
         protected $fillable = [
         'title',
         'description',
